02b9b71d5fced194c43a99b6f9a48e9e2e68e43d
YuYang94724 <the38245691@gmail.com>
2023-12-05 10:36:43 +0800

    feat 學習gradle基本使用

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     kotlin("jvm") version "1.9.0"
     application
+    `maven-publish`
 }
 
 group = "kotlin.travel"
@@ -13,7 +14,15 @@ repositories {
 }
 
 dependencies {
-    testImplementation(kotlin("test"))
+//    testImplementation(kotlin("test"))
+    // Use JUnit Jupiter for testing.
+//    testImplementation("org.junit.jupiter:junit-jupiter:5.9.1")
+    // This dependency is used by the application.
+//    implementation("com.google.guava:guava:32.1.2-jre")
+//    implementation("com.google.guava:guava:30.0-jre")
+
+    testImplementation(libs.junit)
+    implementation(libs.guava)
 }
 
 tasks.test {
@@ -21,9 +30,45 @@ tasks.test {
 }
 
 kotlin {
-    jvmToolchain(8)
+//    jvmToolchain(17)
+    jvmToolchain{
+        languageVersion.set(JavaLanguageVersion.of(17))
+    }
+
 }
 
 application {
     mainClass.set("MainKt")
+}
+
+tasks.register("hello") {
+    doLast {
+        println("Hello!")
+    }
+}
+
+tasks.register("greet") {
+    doLast {
+        println("How are you?")
+    }
+    dependsOn("hello")
+}
+
+tasks.register("good") {
+    doLast {
+        println("You're good! ")
+    }
+    dependsOn("greet")
+}
+
+publishing {
+    publications {
+        create<MavenPublication>("maven") {
+            groupId = "com.gradle.tutorial"
+            artifactId = "tutorial"
+            version = "1.0"
+
+            from(components["java"])
+        }
+    }
 }
